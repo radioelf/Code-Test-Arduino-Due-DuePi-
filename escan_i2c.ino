923d92eac9cf6d5226bae6eb38caa736d8fbbcee
Joan <joan.correo@gmail.com>
2016-12-21 16:12:56 +0100

    Test i2c scanner-> DuePi

---
+++ escan_i2c.ino
@@ -0,0 +1,116 @@
+// Test i2c scanner-> DuePi
+// Arduino Due 3V3 SDA D4 pin 7, SCL D5 pin 8
+// --------------------------------------
+// i2c_scanner
+//
+// Version 1
+//    This program (or code that looks like it)
+//    can be found in many places.
+//    For example on the Arduino.cc forum.
+//    The original author is not know.
+// Version 2, Juni 2012, Using Arduino 1.0.1
+//     Adapted to be as simple as possible by Arduino.cc user Krodal
+// Version 3, Feb 26  2013
+//    V3 by louarnold
+// Version 4, March 3, 2013, Using Arduino 1.0.3
+//    by Arduino.cc user Krodal.
+//    Changes by louarnold removed.
+//    Scanning addresses changed from 0...127 to 1...119,
+//    according to the i2c scanner by Nick Gammon
+//    http://www.gammon.com.au/forum/?id=10896
+// Version 5, March 28, 2013
+//    As version 4, but address scans now to 127.
+//    A sensor seems to use address 120.
+// Version 6, November 27, 2015.
+//    Added waiting for the Leonardo serial communication.
+// 
+//
+// This sketch tests the standard 7-bit addresses
+// Devices with higher bit address might not be seen properly.
+
+// Mod. radioelf for DuePi December 2016
+/* ******************************************************************* 
+  Radioelf - February December 2016
+ http://radioelf.blogspot.com.es/
+ 
+  Copyright (c) 2016 Radioelf.  All rights reserved.
+  Test  Open Source Arduino Due scanner I2C.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>
+
+#define WIRE Wire
+//#define WIRE Wire1
+
+#define ENABL_PIN 31                                                  // PA7  PIN SAM3X 26,  DronPi PIN-> 1,  Max. output current 15mA
+
+#if defined(ARDUINO_SAM_DUE)
+  #define duepi "I2C Scanner"
+#else
+  #error  !!ERROR board Due!! 
+#endif
+void setup()
+{
+  WIRE.begin();
+  Wire.setClock(100000);
+  pinMode(ENABL_PIN, OUTPUT);
+  digitalWrite(ENABL_PIN, HIGH);                                     
+  Serial.begin(115200);
+  while (!Serial);                                                     // Leonardo: wait for serial monitor
+  Serial.println(duepi);
+}
+
+
+void loop()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
+}
